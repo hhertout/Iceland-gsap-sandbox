47164f006051ae869e1bbe76b788216d9e30896b
hhertout <hugues.hertout@gmail.com>
2022-10-29 15:50:47 +0200

    fix compass

--- public/assets/script/gsap.js
+++ public/assets/script/gsap.js
@@ -48,3 +48,17 @@ gsap.to(".home-title", {
     scale: 0.6,
 })
 
+//ANIM COMPASS
+
+const compass = document.querySelector('.compass')
+let compassPosTop = compass.offsetTop
+let compassPosLeft = (window.innerWidth*0.25)
+
+document.addEventListener('mousemove', (e) => {
+
+    gsap.to(compass, {
+        rotate: (e.offsetX - compassPosLeft)*0.03,
+        transformOrigin: "center",
+        ease: "power3",
+    })
+})
